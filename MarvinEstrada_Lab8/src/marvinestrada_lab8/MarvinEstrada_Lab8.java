e8cd669512367280e5da8d28eea7c4068f64a01a
Marvin Estrada <marvinestrada2@gmail.com>
2017-03-17 12:53:22 -0600

    Creacion de proyecto

---
+++ MarvinEstrada_Lab8/src/marvinestrada_lab8/MarvinEstrada_Lab8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marvinestrada_lab8;
+
+/**
+ *
+ * @author Marvin F. Estrada
+ */
+public class MarvinEstrada_Lab8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
